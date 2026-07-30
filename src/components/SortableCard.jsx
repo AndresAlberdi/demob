2e56aa873713825d3308a67d750fd2dac7910aee
Andres Alberdi <andresalberdi@gmail.com>
2026-07-29 22:32:26 -0400

    chore: despliegue (Pruebas/Staging) (demob-1e4a1) y actualizaciones

--- src/components/SortableCard.jsx
+++ src/components/SortableCard.jsx
@@ -5,6 +5,7 @@ import { CSS } from '@dnd-kit/utilities';
 export function SortableCard(props) {
   const { attributes, listeners, setNodeRef, transform, transition } = useSortable({ id: props.id });
   const style = {
+    ...props.style,
     transform: CSS.Transform.toString(transform),
     transition,
     cursor: 'grab',
@@ -15,6 +16,7 @@ export function SortableCard(props) {
     <div
       ref={setNodeRef}
       style={style}
+      className={props.className}
       {...attributes}
       {...listeners}
     >
